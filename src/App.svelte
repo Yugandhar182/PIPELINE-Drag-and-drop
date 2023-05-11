c7c71cb5e53157e30f29ac9e52f50228fe30c076
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-05-11 16:51:48 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,97 +1,90 @@
 <script>
-	// Inspired by https://svelte.dev/repl/810b0f1e16ac4bbd8af8ba25d5e0deff?version=3.4.2.
-	import {flip} from 'svelte/animate';
-	
-	let baskets = [
+  import { flip } from 'svelte/animate';
+
+  let applicants = [
     {
-      "name": "Basket 1",
-      "items": ["Orange", "Pineapple"]
+      "name": "CV shared",
+      "items": ["Chandu", "Sai"]
     },
     {
-      "name": "Basket 2",
-      "items": ["Banana", "Apple"]
+      "name": "Shortlisted",
+      "items": ["Gopi", "Charan"]
     },
-		{
-      "name": "Basket 3",
-      "items": ["GrapeFruit"]
+    {
+      "name": "Rejected",
+      "items": ["Rajesh"]
     }
   ];
-	
-	let hoveringOverBasket;
-	
-	function dragStart(event, basketIndex, itemIndex) {
-		// The data we want to make available when the element is dropped
-    // is the index of the item being dragged and
-    // the index of the basket from which it is leaving.
-		const data = {basketIndex, itemIndex};
-   	event.dataTransfer.setData('text/plain', JSON.stringify(data));
-	}
-	
-	function drop(event, basketIndex) {
-		event.preventDefault();
+
+  let hoveringOverApplicant;
+
+  function dragStart(event, applicantIndex, itemIndex) {
+    const data = { applicantIndex, itemIndex };
+    event.dataTransfer.setData('text/plain', JSON.stringify(data));
+  }
+
+  function drop(event, applicantIndex) {
+    event.preventDefault();
     const json = event.dataTransfer.getData("text/plain");
-		const data = JSON.parse(json);
-		
-		// Remove the item from one basket.
-		// Splice returns an array of the deleted elements, just one in this case.
-		const [item] = baskets[data.basketIndex].items.splice(data.itemIndex, 1);
-		
-    // Add the item to the drop target basket.
-		baskets[basketIndex].items.push(item);
-		baskets = baskets;
-		
-		hoveringOverBasket = null;
-	}
+    const data = JSON.parse(json);
+
+    const [item] = applicants[data.applicantIndex].items.splice(data.itemIndex, 1);
+
+    applicants[applicantIndex].items.push(item);
+    applicants = applicants;
+
+    hoveringOverApplicant = null;
+  }
 </script>
 
-<p>Drag a fruit from one basket to another.</p>
+<p>Candidate Status</p>
 
-{#each baskets as basket, basketIndex (basket)}
+{#each applicants as applicant, applicantIndex (applicant)}
   <div animate:flip>
-    <b>{basket.name}</b>
+    <b>{applicant.name}</b>
     <ul
-	  	class:hovering={hoveringOverBasket === basket.name}
-	    on:dragenter={() => hoveringOverBasket = basket.name}
-      on:dragleave={() => hoveringOverBasket = null}
-  		on:drop={event => drop(event, basketIndex)}
-  		ondragover="return false"
+      class:hovering={hoveringOverApplicant === applicant.name}
+      on:dragenter={() => hoveringOverApplicant = applicant.name}
+      on:dragleave={() => hoveringOverApplicant = null}
+      on:drop={event => drop(event, applicantIndex)}
+      ondragover="return false"
     >
-	    {#each basket.items as item, itemIndex (item)}
-			  <div class="item" animate:flip>
-	      	<li
-	    	    draggable={true}
-		  		  on:dragstart={event => dragStart(event, basketIndex, itemIndex)}
-		    	>
-		      	{item}
-	    	  </li>
-			  </div>
-	    {/each}
+      {#each applicant.items as item, itemIndex (item)}
+        <div class="item" animate:flip>
+          <li
+            draggable={true}
+            on:dragstart={event => dragStart(event, applicantIndex, itemIndex)}
+          >
+            {item}
+          </li>
+        </div>
+      {/each}
     </ul>
   </div>
 {/each}
 
 <style>
-	.hovering {
-		border-color: orange;
-	}
-	.item {
-		display: inline; /* required for flip to work */
-	}
-	li {
-		background-color: lightgray;
-		cursor: pointer;
-		display: inline-block;
-		margin-right: 10px;
-		padding: 10px;
-	}
-	li:hover {
-		background: orange;
-		color: white;
-	}
+  .hovering {
+    border-color: orange;
+  }
+  .item {
+    display: inline;
+  }
+  li {
+    background-color: lightgray;
+    cursor: pointer;
+    display: inline-block;
+    margin-right: 10px;
+    padding: 10px;
+  }
+  li:hover {
+    background: orange;
+    color: white;
+  }
   ul {
-		border: solid lightgray 1px;
-		display: flex; /* required for drag & drop to work when .item display is inline */
-		height: 40px; /* needed when empty */
-		padding: 10px;
-	}
+    border: solid lightgray 1px;
+    display: flex;
+    height: 40px;
+    padding: 10px;
+  }
 </style>
