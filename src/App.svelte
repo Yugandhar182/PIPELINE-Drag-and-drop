c798089206d6307d317d7b18cc1b592bb232eecc
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-05-11 16:35:39 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,100 +1,97 @@
 <script>
-	import { flip } from 'svelte/animate';
-  
-	let CANDIDATES = [
-	  {
-		"name": "Applied candidates",
-		"items": ["Sai", "Chandu", "Dilli", "Charan", "Gopi", "Koti", "Vinoth"]
-	  },
-	  {
-		"name": "Selected Candiadates",
-		"items": []
-	  },
-	  {
-		"name": "Rejected Candidates",
-		"items": []
-	  }
-	];
-  
-	let hoveringOverCANDIDATES;
-	let dropIndex = null;
-  
-	function dragStart(event, CANDIDATESIndex, itemIndex) {
+	// Inspired by https://svelte.dev/repl/810b0f1e16ac4bbd8af8ba25d5e0deff?version=3.4.2.
+	import {flip} from 'svelte/animate';
 	
-	  event.dataTransfer.setData('text/plain', `${CANDIDATESIndex}:${itemIndex}`);
+	let baskets = [
+    {
+      "name": "Basket 1",
+      "items": ["Orange", "Pineapple"]
+    },
+    {
+      "name": "Basket 2",
+      "items": ["Banana", "Apple"]
+    },
+		{
+      "name": "Basket 3",
+      "items": ["GrapeFruit"]
+    }
+  ];
+	
+	let hoveringOverBasket;
+	
+	function dragStart(event, basketIndex, itemIndex) {
+		// The data we want to make available when the element is dropped
+    // is the index of the item being dragged and
+    // the index of the basket from which it is leaving.
+		const data = {basketIndex, itemIndex};
+   	event.dataTransfer.setData('text/plain', JSON.stringify(data));
 	}
-  
-	function drop(event, CANDIDATESIndex) {
-	  event.preventDefault();
-	  const [sourceIndex, itemIndex] = event.dataTransfer.getData('text/plain').split(':');
-  
-	  const [item] = CANDIDATES[sourceIndex].items.splice(itemIndex, 1);
-  
-	  const items = CANDIDATES[CANDIDATESIndex].items;
-	  items.splice(dropIndex ?? items.length, 0, item);
-	  CANDIDATES = CANDIDATES;
-  
-	  hoveringOverCANDIDATES = null;
+	
+	function drop(event, basketIndex) {
+		event.preventDefault();
+    const json = event.dataTransfer.getData("text/plain");
+		const data = JSON.parse(json);
+		
+		// Remove the item from one basket.
+		// Splice returns an array of the deleted elements, just one in this case.
+		const [item] = baskets[data.basketIndex].items.splice(data.itemIndex, 1);
+		
+    // Add the item to the drop target basket.
+		baskets[basketIndex].items.push(item);
+		baskets = baskets;
+		
+		hoveringOverBasket = null;
 	}
-  
-	function dragOver(e) {
-	  dropIndex = null;
-	  const itemElement = e.target.closest('[data-item]');
-	  if (itemElement == null)
-		return;
-  
-	  const { CANDIDATES, item } = itemElement.dataset;
-	  const targetCANDIDATES = CANDIDATES.find(c => c.name == CANDIDATES);
-	  dropIndex = targetCANDIDATES.items.indexOf(item);
-	}
-  </script>
-  
-  <p>CANDIDATES Status</p>
-  
-  {#each CANDIDATES as CANDIDATES, CANDIDATESIndex (CANDIDATES)}
-	<div animate:flip>
-	  <b>{CANDIDATES.name}</b>
-	  <ul
-		class:hovering={hoveringOverCANDIDATES === CANDIDATES.name}
-		on:dragenter={() => hoveringOverCANDIDATES = CANDIDATES.name}
-		on:dragleave={() => hoveringOverCANDIDATES = null}
-		on:drop={event => drop(event, CANDIDATESIndex)}
-		on:dragover|preventDefault={dragOver}>
-		{#each CANDIDATES.items as item, itemIndex (item)}
-		  <div class="item" data-CANDIDATES={CANDIDATES.name} data-item={item} animate:flip>
-			<li draggable={true} on:dragstart={event => dragStart(event, CANDIDATESIndex, itemIndex)}>
-			  {item}
-			</li>
-		  </div>
-		{/each}
-	  </ul>
-	</div>
-  {/each}
-  
-  <style>
+</script>
+
+<p>Drag a fruit from one basket to another.</p>
+
+{#each baskets as basket, basketIndex (basket)}
+  <div animate:flip>
+    <b>{basket.name}</b>
+    <ul
+	  	class:hovering={hoveringOverBasket === basket.name}
+	    on:dragenter={() => hoveringOverBasket = basket.name}
+      on:dragleave={() => hoveringOverBasket = null}
+  		on:drop={event => drop(event, basketIndex)}
+  		ondragover="return false"
+    >
+	    {#each basket.items as item, itemIndex (item)}
+			  <div class="item" animate:flip>
+	      	<li
+	    	    draggable={true}
+		  		  on:dragstart={event => dragStart(event, basketIndex, itemIndex)}
+		    	>
+		      	{item}
+	    	  </li>
+			  </div>
+	    {/each}
+    </ul>
+  </div>
+{/each}
+
+<style>
 	.hovering {
-	  border-color: orange;
+		border-color: orange;
 	}
 	.item {
-	  display: inline;
+		display: inline; /* required for flip to work */
 	}
 	li {
-	  background-color: lightgray;
-	  cursor: pointer;
-	  display: inline-block;
-	  margin-right: 10px;
-	  padding: 10px;
+		background-color: lightgray;
+		cursor: pointer;
+		display: inline-block;
+		margin-right: 10px;
+		padding: 10px;
 	}
 	li:hover {
-	  background: orange;
-	  color: rgb(224, 15, 15);
+		background: orange;
+		color: white;
 	}
-	ul {
-	  border: solid lightgray 1px;
-	  display: flex;
-	  height: 40px;
-	  padding: 10px;
+  ul {
+		border: solid lightgray 1px;
+		display: flex; /* required for drag & drop to work when .item display is inline */
+		height: 40px; /* needed when empty */
+		padding: 10px;
 	}
-  </style>
-  
-  
+</style>
